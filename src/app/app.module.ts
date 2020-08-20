3fae1e068c0d39abeabca41c23866beed8033886
heshan <heshanu97@gmail.com>
2020-08-20 12:48:48 +0530

    add route and path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule,Routes} from '@angular/router';
+import {HttpModule} from '@angular/http';
+//import {FormsModule, FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { RegsiterComponent } from './regsiter/regsiter.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
+
+const appRoutes:Routes=[
+  {path:'',component:HomeComponent},
+  {path:'register',component:RegsiterComponent},
+  {path:'login',component:LoginComponent},
+  {path:'navbar',component:NavbarComponent},
+  {path:'profile',component:ProfileComponent},
+  {path:'dashboard',component:DashboardComponent},
+  
+]
 
 @NgModule({
   declarations: [
@@ -20,7 +34,8 @@ import { ProfileComponent } from './profile/profile.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
